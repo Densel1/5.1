3771ccbbb71d11ab3252932e11cc84290552af0e
Igor Kastyrkin <tremen@yandex.ru>
2023-07-03 19:47:47 +0300

    initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	vector <int> v = { 4, 7, 9, 14, 12 };
+	cout << "Входные данные:";
+	for_each(v.begin(), v.end(),
+		[=](int i) mutable { cout << " " << i ;  });
+
+	cout << endl << "Выходные данные:";
+	for_each(v.begin(), v.end(),
+		[=](int i) mutable {int d = 3, j = 0; cout << " " << i+(i%2!=0)*(d-1)*i;  });
+
+	return 0;
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>
+#include <variant>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+variant<int, string, vector<int>> get_variant() {
+	srand(time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	variant<int, string, vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+
+int main()
+{
+
+	variant<int, string, vector<int>> myVariant = get_variant();
+
+	if (holds_alternative<int>(myVariant))
+	{
+		auto my_integer = get<int>(myVariant);
+		cout << " " << my_integer*2 << endl;;
+	}
+
+	else if (holds_alternative<string>(myVariant))
+	{
+		auto str = get<string>(myVariant);
+		cout << " " << str << endl;
+	}
+
+	else
+	{
+		vector<int> my_vector = get<vector<int>>(myVariant);
+		for_each(my_vector.begin(), my_vector.end(),
+			[=](int i) mutable { cout << " " << i;  });
+	}
+
+	return 0;
+}
+
